9c085a4a4cc783ad330add43b1a09608ffd04167
Jose Blanca <jblanca@upv.es>
2025-04-24 10:00:26 +0200

    implement compres and decompres fastq

--- src/fastq_compress/fastq.py
+++ src/fastq_compress/fastq.py
@@ -3,6 +3,8 @@ from collections import namedtuple
 
 from more_itertools import chunked
 
+from fastq_compress.compressor import write_compressed_file, read_compressed_file
+
 Seq = namedtuple("Seq", ["name_line", "seq", "qual"])
 
 
@@ -16,3 +18,36 @@ def _group_seqs(fhand: BinaryIO) -> Iterator[Seq]:
 def iterate_fastq_chunks(fhand: BinaryIO, n_seqs: int) -> Iterator[Seq]:
     seqs = _group_seqs(fhand)
     return chunked(seqs, n_seqs)
+
+
+def _columnize_fastq_chunk(chunk):
+    name_lines, seqs, quals = [], [], []
+    for seq in chunk:
+        name_lines.append(seq.name_line.rstrip(b"\n"))
+        seqs.append(seq.seq.rstrip(b"\n"))
+        quals.append(seq.qual.rstrip(b"\n"))
+    return name_lines, seqs, quals
+
+
+def _decolumnize_fastq_chunk(chunk):
+    seqs = []
+    for name, seq, qual in zip(*chunk):
+        seqs.append(Seq(name, seq, qual))
+    return seqs
+
+
+def compress_fastq(fhand: BinaryIO, compressed_fhand: BinaryIO, n_seqs_in_chunk: int):
+    chunks = iterate_fastq_chunks(fhand, n_seqs_in_chunk)
+    chunks = map(_columnize_fastq_chunk, chunks)
+    write_compressed_file(compressed_fhand, chunks)
+
+
+def decompress_fastq(compressed_fhand: BinaryIO, decompressed_fhand: BinaryIO):
+    chunks = read_compressed_file(compressed_fhand)
+    chunks = map(_decolumnize_fastq_chunk, chunks)
+
+    for chunk in chunks:
+        for seq in chunk:
+            to_write = b"\n".join([seq.name_line, seq.seq, b"+", seq.qual])
+            decompressed_fhand.write(to_write)
+            decompressed_fhand.write(b"\n")
--- tests/test_iterate_fastq.py
+++ tests/test_iterate_fastq.py
@@ -1,19 +1,20 @@
 from io import BytesIO
 
-from fastq_compress.fastq import iterate_fastq_chunks
+from fastq_compress.fastq import iterate_fastq_chunks, compress_fastq, decompress_fastq
 
 FASTQ1 = b"""@SEQ1
-GATTTGGGGTTCAAAGCAGTATCGATCAAATAGTAAATCCATTTGTTCAACTCACAGTTT
+AATTTGGGGTTCAAAGCAGTATCGATCAAATAGTAAATCCATTTGTTCAACTCACAGTTT
 +
 !''*((((***+))%%%++)(%%%%).1***-+*''))**55CCF>>>>>>CCCCCCC65
 @SEQ2
 GATTTGGGGTTCAAAGCAGTATCGATCAAATAGTAAATCCATTTGTTCAACTCACAGTTT
 +
 !''*((((***+))%%%++)(%%%%).1***-+*''))**55CCF>>>>>>CCCCCCC65
-@SEQ2
-GATTTGGGGTTCAAAGCAGTATCGATCAAATAGTAAATCCATTTGTTCAACTCACAGTTT
+@SEQ3
+CATTTGGGGTTCAAAGCAGTATCGATCAAATAGTAAATCCATTTGTTCAACTCACAGTTT
 +
-!''*((((***+))%%%++)(%%%%).1***-+*''))**55CCF>>>>>>CCCCCCC65"""
+!''*((((***+))%%%++)(%%%%).1***-+*''))**55CCF>>>>>>CCCCCCC65
+"""
 
 
 def test_fastq_chunker():
@@ -23,3 +24,15 @@ def test_fastq_chunker():
     assert len(chunks) == 2
     assert len(chunks[0]) == 2
     assert len(chunks[1]) == 1
+
+
+def test_compress_fastq():
+    fhand = BytesIO(FASTQ1)
+    compressed_fhand = BytesIO()
+    compress_fastq(fhand, compressed_fhand, n_seqs_in_chunk=2)
+
+    compressed_fhand.seek(0)
+    decompressed_fhand = BytesIO()
+    decompress_fastq(compressed_fhand, decompressed_fhand)
+    decompressed_fhand.seek(0)
+    assert decompressed_fhand.read() == FASTQ1
